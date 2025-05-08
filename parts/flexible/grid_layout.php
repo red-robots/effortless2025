15b0337739fd29ebde5027d07b885a8bd1374e10
Lisa <lisaqdebona@gmail.com>
2025-05-08 03:35:37 -0400

    Added new flexible content fields

---
+++ parts/flexible/grid_layout.php
@@ -0,0 +1,57 @@
+<?php if( get_row_layout() == 'grid_layout' ) {
+  $add_intro = get_sub_field('add_intro');
+  $numcol = get_sub_field('columns_per_row');
+  $perRow = ($numcol) ? $numcol : 3;
+  $items = get_sub_field('items');
+  $intro_text = '';
+  if($add_intro) {
+    $intro_text = get_sub_field('section_intro');
+  }
+  $items = get_sub_field('items');
+  if( $items ) { ?>
+  <section data-group="<?php echo get_row_layout() ?>" class="repeatable repeatable--<?php echo get_row_layout() ?> repeatable--<?php echo get_row_layout() ?>-<?php echo $ctr ?>">
+    <?php if ($intro_text) { ?>
+    <div class="intro-wrapper">
+      <div class="wrapper">
+        <?php echo anti_email_spam($intro_text); ?>
+      </div>
+    </div>
+    <?php } ?>
+
+    <?php if ($items) { ?>
+    <div class="featuredArticles grid-posts numcol-<?php echo $numcol ?>">
+      <div class="flexwrap">
+        <?php foreach ($items as $a) { 
+          $image = $a['image'];
+          $text = $a['column_description'];
+          $btn = $a['button'];
+          $btnLink = (isset($btn['url'])) ? $btn['url'] : '';
+          $btnName = (isset($btn['title'])) ? $btn['title'] : '';
+          $btnTarget = (isset($btn['target'])) ? $btn['target'] : '';
+          $hasImage = ($image) ? 'has-image':'no-image';
+        ?>
+        <div class="infobox">
+          <figure class="<?php echo $hasImage ?>">
+            <?php if ($image) { ?>
+              <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['title'] ?>">
+            <?php } ?>
+          </figure>
+
+          <div class="details">
+            <?php if ($text) { ?>
+              <div class="text"><?php echo anti_email_spam($text); ?></div>
+            <?php } ?>
+            <?php if ($btnLink && $btnName) { ?>
+              <div class="button-wrap">
+                <a href="<?php echo $btnLink ?>" target="<?php echo $btnTarget ?>" class="button"><?php echo $btnName ?></a>
+              </div>
+            <?php } ?>
+          </div>
+        </div>
+      <?php } ?>
+      </div>
+    </div>
+    <?php } ?>
+  </section>
+  <?php } ?>
+<?php } ?>
--- parts/hero.php
+++ parts/hero.php
@@ -12,60 +12,64 @@ if(!$hero_text) {
 if($hero_type=='image') {
   if($hero_image) { ?>
   <section id="hero" class="hero-image">
-    <div class="hero-content">
-      <div class="heroImage" style="background-image:url('<?php echo $hero_image['url'] ?>')"></div>
-      <?php if ($hero_text) { ?>
-        <div class="heroText">
-          <div class="textwrap">
-            <?php echo anti_email_spam($hero_text); ?>
+    <div class="wrapper">
+      <div class="hero-content">
+        <div class="heroImage" style="background-image:url('<?php echo $hero_image['url'] ?>')"></div>
+        <?php if ($hero_text) { ?>
+          <div class="heroText">
+            <div class="textwrap">
+              <?php echo anti_email_spam($hero_text); ?>
+            </div>
           </div>
-        </div>
-      <?php } ?>
+        <?php } ?>
+      </div>
     </div>
   </section>
   <?php } ?>
 <?php } else if( $hero_type=='video' ) { ?>
   <?php if ($videoURL) { ?>
   <section id="hero" class="hero-video">
-    <?php if ( (strpos( strtolower($videoURL), 'youtube.com') !== false) || (strpos( strtolower($videoURL), 'youtu.b') !== false)  ) { 
-    $youtubeId = extractYoutubeId($videoURL);
-    $embed_url = 'https://www.youtube.com/embed/'.$youtubeId.'?version=3&rel=0&loop=0'; 
-    $mainImage = 'https://i.ytimg.com/vi/'.$youtubeId.'/maxresdefault.jpg'; ?>
-    <div class="hero-content">
-      <div class="videoWrapper iframe-youtube">
-        <iframe class="videoIframe" data-vid="youtube" src="<?php echo $embed_url; ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="wrapper">
+      <?php if ( (strpos( strtolower($videoURL), 'youtube.com') !== false) || (strpos( strtolower($videoURL), 'youtu.b') !== false)  ) { 
+      $youtubeId = extractYoutubeId($videoURL);
+      $embed_url = 'https://www.youtube.com/embed/'.$youtubeId.'?version=3&rel=0&loop=0'; 
+      $mainImage = 'https://i.ytimg.com/vi/'.$youtubeId.'/maxresdefault.jpg'; ?>
+      <div class="hero-content">
+        <div class="videoWrapper iframe-youtube">
+          <iframe class="videoIframe" data-vid="youtube" src="<?php echo $embed_url; ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <?php if ($hero_text) { ?>
+          <div class="heroText">
+            <div class="textwrap">
+              <?php echo anti_email_spam($hero_text); ?>
+            </div>
+          </div>
+        <?php } ?>
       </div>
-      <?php if ($hero_text) { ?>
+      <?php } ?>
+
+
+      <?php if( strpos( strtolower($videoURL), 'vimeo.com') !== false ) { 
+      $vimeo_parts = explode("/",$videoURL);
+      $parts = ($vimeo_parts && array_filter($vimeo_parts) ) ? array_filter($vimeo_parts) : '';
+      $vimeoId = ($parts) ?  preg_replace('/\s+/', '', end($parts)) : '';
+      $vimeoData = ($vimeoId) ? get_vimeo_data($vimeoId) : '';
+      $data = json_decode( file_get_contents( 'https://vimeo.com/api/oembed.json?url=' . $videoURL ) );
+      $vimeoImage = ($data) ? $data->thumbnail_url : '';  ?>
+      <div class="hero-content">
+        <div class="videoWrapper iframe-vimeo">
+          <iframe class="videoIframe" data-vid="vimeo" src="https://player.vimeo.com/video/<?php echo $vimeoId?>" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        </div>
+        <?php if ($hero_text) { ?>
         <div class="heroText">
           <div class="textwrap">
             <?php echo anti_email_spam($hero_text); ?>
           </div>
         </div>
-      <?php } ?>
-    </div>
-    <?php } ?>
-
-
-    <?php if( strpos( strtolower($videoURL), 'vimeo.com') !== false ) { 
-    $vimeo_parts = explode("/",$videoURL);
-    $parts = ($vimeo_parts && array_filter($vimeo_parts) ) ? array_filter($vimeo_parts) : '';
-    $vimeoId = ($parts) ?  preg_replace('/\s+/', '', end($parts)) : '';
-    $vimeoData = ($vimeoId) ? get_vimeo_data($vimeoId) : '';
-    $data = json_decode( file_get_contents( 'https://vimeo.com/api/oembed.json?url=' . $videoURL ) );
-    $vimeoImage = ($data) ? $data->thumbnail_url : '';  ?>
-    <div class="hero-content">
-      <div class="videoWrapper iframe-vimeo">
-        <iframe class="videoIframe" data-vid="vimeo" src="https://player.vimeo.com/video/<?php echo $vimeoId?>" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-      </div>
-      <?php if ($hero_text) { ?>
-      <div class="heroText">
-        <div class="textwrap">
-          <?php echo anti_email_spam($hero_text); ?>
-        </div>
+        <?php } ?>
       </div>
       <?php } ?>
     </div>
-    <?php } ?>
   </section>
   <?php } ?>
 <?php } ?>
