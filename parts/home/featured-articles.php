97cafb5417b58603f26b422c6d4ed97b514cfb8d
Lisa <lisaqdebona@gmail.com>
2025-06-25 03:46:56 -0400

    Made changes on the shop page

--- parts/home/featured-articles.php
+++ parts/home/featured-articles.php
@@ -1,14 +1,24 @@
 <?php  
 $featured_intro = get_field('featured_intro');
+$featured_intro_bg = get_field('featured_intro_background');
+$featured_intro_hasbg = ($featured_intro_bg) ? 'has-graphic-bg':'no-graphic-bg';
 $featured_articles = get_field('featured_articles');
 if($featured_articles) { ?>
-<section class="featuredArticles">
-  <div class="wrapper">
-    <?php if ($featured_intro) { ?>
+<section class="featuredArticles <?php echo $featured_intro_hasbg ?>">
+  <?php if ($featured_intro) { ?>
+    <?php if ($featured_intro_bg) { ?>
+    <div class="intro-wrapper intro-graphic">
+      <div class="wrapper text"><?php echo anti_email_spam($featured_intro); ?></div>
+    </div> 
+    <?php } else { ?>
     <div class="intro">
-      <?php echo anti_email_spam($featured_intro); ?>
+      <div class="wrapper">
+        <?php echo anti_email_spam($featured_intro); ?>
+      </div>
     </div> 
     <?php } ?>
+  <?php } ?>
+  <div class="wrapper">
     <div class="flexwrap">
       <?php foreach ($featured_articles as $a) { 
         $image = $a['image'];
